de7f2a960c5905307253fa230d6ce454b475e9b8
Tomasz Kras <tkras@future-processing.com>
2020-05-07 10:56:45 +0200

    Updates

---
+++ precache-manifest.80465c7ace50c0d78e920f3133d5a8c0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7b864dc41a9abd8940f16d2e45abb3f",
+    "url": "/support-timesheets/index.html"
+  },
+  {
+    "revision": "eb66ab3355c8abe6990e",
+    "url": "/support-timesheets/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "a404d800d506d4b75b13",
+    "url": "/support-timesheets/static/css/main.b7ad9a96.chunk.css"
+  },
+  {
+    "revision": "eb66ab3355c8abe6990e",
+    "url": "/support-timesheets/static/js/2.3daf9734.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/support-timesheets/static/js/2.3daf9734.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a404d800d506d4b75b13",
+    "url": "/support-timesheets/static/js/main.035825b2.chunk.js"
+  },
+  {
+    "revision": "f5f57fe2a883cc02fcfe",
+    "url": "/support-timesheets/static/js/runtime-main.6bc5f4f9.js"
+  }
+]);
